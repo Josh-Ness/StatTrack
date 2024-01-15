7c88d19fffba11495bb9f0ae61d1599bab00549c
Josh <joshua.s.ness@ndsu.edu>
2024-01-14 18:27:33 -0600

    Fixed Flutter Initialization

--- StatTrackApp/src/main/kotlin/Main.kt
+++
@@ -1,7 +0,0 @@
-fun main(args: Array<String>) {
-    println("This is just a basic starter template for this kotlin file. This has not yet been developed")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
-}
---
+++ stat_track_flutter/android/app/src/main/kotlin/com/example/stat_track_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.stat_track_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
